eb6e5f7dd42acc56a83a7f3e3b0ff94d97dd7e23
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-04-02 20:34:52 -0300

    :sparkles: adds cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors"
 import "./database";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 export default app;
